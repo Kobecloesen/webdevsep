d2c63a1ad95ac42c97143fa89d1ea4910e61967e
Kobecloesen <71632473+Kobecloesen@users.noreply.github.com>
2020-11-10 13:26:13 +0100

    init

---
+++ htmlandcss/Javascript/test1/app.js
@@ -0,0 +1,29 @@
+
+let students = {
+    Firstname: "Kobe ", 
+    Lastname: "Cloesen",
+    Age: 25,
+    email: "Cloesen.kobe@gmail.com",
+    book: ""
+
+}
+
+
+
+
+function testfunction(myparametername){
+    console.log(myparametername.Firstname),
+    console.log(myparametername.Lastname),
+    console.log(myparametername.Age),
+    console.log(myparametername.email)
+    console.log(myparametername)
+    console.log(myparametername.book)
+}
+
+testfunction(students);
+
+
+
+
+
+
