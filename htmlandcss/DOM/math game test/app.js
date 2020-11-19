46d6badaf749bbf9fa53e42275106f8f8060974c
Kobecloesen <71632473+Kobecloesen@users.noreply.github.com>
2020-11-19 09:06:03 +0100

    update

--- htmlandcss/DOM/math game test/app.js
+++ htmlandcss/DOM/math game test/app.js
@@ -1,7 +1,60 @@
-var number1 = Math.floor(Math.random()*25)+1;
-var number2 = Math.floor(Math.random()*25)+1;
-var guess = document.getElementById("question");
-var result = Number(number1)+operators[Math.round(Math.random()*4)-1]+Number(number2);
+let operators = ["+","-","*","/"]
 
 
-document.getElementById("question").value = result;
+
+
+var number1 = Math.round(Math.random()*99);
+var number2 = Math.round(Math.random()*99);
+var operator = operators[Math.round(Math.random()*3)];
+let result;
+var answer = document.getElementById('answer');
+
+
+
+
+question.innerHTML = number1 + operator + number2;
+
+
+console.log(number1)
+console.log(operator)
+console.log(number2)
+
+
+// | replaced by eval() |
+// V                    V
+
+// function calc(){
+//     if(operator === "+"){
+//         return number1 + number2
+//     }else if (operator === "-"){
+//         return number1 - number2
+//     }else if (operator === "*"){
+//        return number1 * number2
+//     }else{
+//         return number1 / number2
+//     }  
+// }
+
+// result = calc();
+
+
+function calcCheck(){
+    console.log(Number(answer.value)) //this one
+   // console.log(answer.value) not this one
+    let correct_answer = eval(number1 + operator + number2)
+    console.log(correct_answer)
+    if(correct_answer == Number(answer.value)){
+        console.log('correct')    
+        correct.innerHTML++
+        
+    }else{
+        console.log('wrong')  
+        incorrect.innerHTML++
+        
+    }
+    
+}
+
+
+check.addEventListener('click',calcCheck)
+
