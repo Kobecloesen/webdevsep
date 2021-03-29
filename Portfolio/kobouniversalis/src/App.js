59d7e190f87431337aa07eb85acaaef411ce7f96
Kobecloesen <71632473+Kobecloesen@users.noreply.github.com>
2021-03-29 12:42:52 +0200

    update

--- Portfolio/kobouniversalis/src/App.js
+++ Portfolio/kobouniversalis/src/App.js
@@ -1,12 +1,19 @@
 
-import './App.css'
+
+import Navbar from './NavBar';
+import Home from './Home'
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <p>blABLAbla</p>
+        <Home/>
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ Portfolio/kobouniversalis/src/NavBar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar justify-content-md-center">
+           <a href="/"><h1>Kobo Universalis</h1></a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
