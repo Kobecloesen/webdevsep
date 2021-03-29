80013d8f96392ea739b181bac86c0afe4c50867e
Kobecloesen <71632473+Kobecloesen@users.noreply.github.com>
2021-03-29 14:06:50 +0200

    update

--- Portfolio/kobouniversalis/src/Footer.js
+++ Portfolio/kobouniversalis/src/Footer.js
@@ -2,6 +2,8 @@ import "./Footer.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FormFile from 'react-bootstrap/FormFile';
 
 
 const Footer = () => {
